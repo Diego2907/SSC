e9096e206f5e181526bef05ec39323d759d0e51f
Diego2907 <dverduzco10@ucol.mx>
2025-10-30 18:48:59 -0600

    cambio de ubicacion de archivos

--- Client/src/features/shared/layouts/MainLayout.tsx
+++ Client/src/features/shared/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Header from "../features/home/components/Header";
+import Header from "../../features/home/components/Header";
 
 const MainLayout = () => {
   return (
