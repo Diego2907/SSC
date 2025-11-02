9793a94ac399dd0c780e6906ba6bc10ea2aa060a
DenisseSolz <dsolorzano3@ucol.mx>
2025-11-02 13:05:52 -0600

    Tenia dos carpeta de MainLayout, deje solo deje src/features/shared/layouts

--- Client/src/features/shared/layouts/MainLayout.tsx
+++ Client/src/features/shared/layouts/MainLayout.tsx
@@ -1,9 +1,5 @@
 import { Outlet } from "react-router-dom";
-<<<<<<< HEAD:Client/src/layouts/MainLayout.tsx
-import Header from "../features/home/components/Header";
-=======
 import Header from "../../home/components/Header";
->>>>>>> d3327d521f8617a9f7f95600582885bc9424cedf:Client/src/features/shared/layouts/MainLayout.tsx
 
 const MainLayout = () => {
   return (
