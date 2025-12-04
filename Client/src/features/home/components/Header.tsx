dba8a66275a0547fb2d29ad17ee7c5641e5b315e
DenisseSolz <dsolorzano3@ucol.mx>
2025-12-03 22:34:44 -0600

    Implement login modal functionality in Header component

--- Client/src/features/home/components/Header.tsx
+++ Client/src/features/home/components/Header.tsx
@@ -2,8 +2,15 @@ import Navbar from "../../shared/components/Navbar";
 import Button from "../../shared/components/Button";
 import Image from "../../shared/components/Image";
 import logo from "../assets/img/Logo.webp";
+import Modal from "./sectionComponents/login-modal";
+import { useState } from "react";
 
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const abrirModal = () => setIsModalOpen(true);
+  const cerrarModal = () => setIsModalOpen(false);
+
   return (
     <header className="sticky top-0 z-50 flex items-center justify-between w-full px-4 sm:px-6 md:px-10 lg:px-20 bg-white dark:bg-gray-900 h-16 sm:h-20 md:h-24 shadow-lg transition-colors duration-300">
       <Image
@@ -23,8 +30,10 @@ const Header = () => {
       />
       <Button
         ClassNameButton="hidden sm:block font-poppins text-[#1D4289] text-xs sm:text-sm lg:text-[15px] font-semibold bg-[#FFE900] w-auto px-4 lg:w-[260px] h-10 lg:h-[51px] border-black rounded-[10px] shadow-lg cursor-pointer transition-all duration-200 ease-in-out hover:-translate-y-1 hover:scale-105 hover:bg-[#E6D200] hover:text-[#14356E] hover:font-bold"
+        onClick={abrirModal}
         textButton="Iniciar sesión | Registrarse"
       />
+      <Modal isOpen={isModalOpen} onClose={cerrarModal} />
     </header>
   );
 };
