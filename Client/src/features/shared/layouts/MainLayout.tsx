8c4657f207285d9dc75e1100e404400979dc20dd
DenisseSolz <dsolorzano3@ucol.mx>
2025-12-17 15:01:08 -0600

    Remove Header component from MainLayout

--- Client/src/features/shared/layouts/MainLayout.tsx
+++ Client/src/features/shared/layouts/MainLayout.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Header from "../../home/components/Header";
 
 const MainLayout = () => {
   return (
     <div className="relative">
-      <Header />
       <Outlet />
     </div>
   );
