dd907a0c3c8681e24806d063891737413c554bb9
DenisseSolz <dsolorzano3@ucol.mx>
2025-12-18 12:32:09 -0600

    Add user routes for settings and profile pages with UserLayout

--- Client/src/app/routes/index.tsx
+++ Client/src/app/routes/index.tsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../../features/shared/layouts/MainLayout";
+import UserLayout from "../../features/shared/layouts/UserLayout";
 import HomePage from "../../features/home/pages/HomePage";
 import RegisterPage from "../../features/home/pages/ResgisterPage";
 import LoginPage from "../../features/home/pages/LoginPage";
+import UserProfilePage from "../../features/users/pages/UserProfilePage";
+import UserSettingsPage from "../../features/users/pages/UserSettingsPage";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +23,21 @@ const router = createBrowserRouter([
       {
         path: "login", // Ruta para registro -> "/register"
         element: <LoginPage />,
+      },
+    ],
+  },
+
+  {
+    path: "/user",
+    element: <UserLayout />,
+    children: [
+      {
+        path: "settings",
+        element: <UserSettingsPage />,
+      },
+      {
+        path: "profile",
+        element: <UserProfilePage />,
       },
     ],
   },
