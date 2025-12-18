1e76333382e31bb4c9719a741791d12091561394
Diego2907 <dverduzco10@ucol.mx>
2025-12-18 00:35:22 -0600

    add domicilio routes

---
+++ server/src/modules/users/router/domicilio.route.ts
@@ -0,0 +1,38 @@
+/**
+ * ? agregar, actualizar, eliminar,
+ * ? ver todos, ver uno domicilios de usuario
+ */
+import { Router } from "express";
+import * as domicilioController from "../controllers/domicilio.controller.js";
+import {
+	validarAgregarDomicilio,
+	validarActualizarDomicilio,
+} from "../validators/domicilio.validation.js";
+import { authenticate } from "../../auth/middleware/auth.middleware.js";
+const router = Router();
+
+router.post(
+	"/agregar",
+	authenticate,
+	validarAgregarDomicilio,
+	domicilioController.agregarDomicilio
+);
+router.put(
+	"/actualizar",
+	authenticate,
+	validarActualizarDomicilio,
+	domicilioController.actualizarDomicilio
+);
+router.delete(
+	"/eliminar/:idDomicilio",
+	authenticate,
+	domicilioController.eliminarDomicilio
+);
+router.get(
+	"/verTodos/:idUsuario",
+	authenticate,
+	domicilioController.verTodosDomicilios
+);
+router.get("/:idDomicilio", authenticate, domicilioController.verDomicilio);
+
+export default router;
