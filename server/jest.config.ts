fd51fbe37331801a4eac95b77ab6847e0064ce95
Diego2907 <dverduzco10@ucol.mx>
2025-12-17 19:10:57 -0600

    Añadir jest

---
+++ server/jest.config.ts
@@ -0,0 +1,26 @@
+export default {
+	preset: "ts-jest/presets/default-esm",
+	testEnvironment: "node",
+	extensionsToTreatAsEsm: [".ts"],
+	moduleNameMapper: {
+		"^(\\.{1,2}/.*)\\.js$": "$1",
+	},
+	transform: {
+		"^.+\\.tsx?$": [
+			"ts-jest",
+			{
+				useESM: true,
+			},
+		],
+	},
+	testMatch: ["**/tests/**/*.test.ts", "**/tests/**/*.spec.ts"],
+	collectCoverageFrom: [
+		"src/**/*.ts",
+		"!src/**/*.d.ts",
+		"!src/**/*.interface.ts",
+	],
+	coverageDirectory: "coverage",
+	coverageReporters: ["text", "lcov", "html"],
+	roots: ["<rootDir>/src", "<rootDir>/tests"],
+	moduleDirectories: ["node_modules", "src"],
+};
